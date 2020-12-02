44a1d3b714133d14eaceba6a4148a819b90f5530
Darya Volokhova <71918075+oh-yikes@users.noreply.github.com>
2020-12-02 12:26:30 +0500

    Объяснительный компонент

---
+++ 2 лаб/Expert_system/COE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expert_system
+{
+	class COE       // component of the explanation (компонента объяснения)
+	{
+		public void PrintOutCurrentsFacts(List<CurrentFact> cf)
+		{
+			List<string> Fact_names = new List<string>();
+			List<string> Fact_values = new List<string>();
+			for(int i = 0; i < cf.Count; i++)
+			{
+				Fact_names.Add(cf[i].Name);
+				Fact_values.Add(cf[i].Value);
+			}
+			Form2 f = new Form2(Fact_names, Fact_values);
+
+			f.ShowDialog();
+		}
+	}
+}
