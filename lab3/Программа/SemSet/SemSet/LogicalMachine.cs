8cd37c6936619e4950dfdf0f1a34c486a6ae67ae
Eugene <71206777+GribanovEvgeny@users.noreply.github.com>
2021-04-19 18:33:07 +0500

    Add files via upload

---
+++ lab3/Программа/SemSet/SemSet/LogicalMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemSet
+{
+	class LogicalMachine
+	{
+		public readonly KnowledgeBase kb = new KnowledgeBase();
+		public LogicalMachine()
+		{
+			kb.GenerateKnowledgeBase();
+		}
+		public string GetAnswer(Entity parent, TypeOfConnection type_of_c, Entity child)
+		{
+			foreach (Connection connection in parent.Connections)
+				if (connection.Type == type_of_c)
+				{
+					if (connection.Child == child)
+						return "да";
+					else
+						if (GetAnswer(connection.Child, type_of_c, child) == "да")
+						return "да";
+				}
+			return "нет";
+		}
+	}
+}
